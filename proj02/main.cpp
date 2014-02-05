cb58580b55dd9d79987be1e7b468556854b87946
njd911 <njdhockeydude@gmail.com>
2014-02-05 11:48:27 -0500

    Initial Commit

---
+++ proj02/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  proj02
+//
+//  Created by Nick Durak on 2/5/14.
+//  Copyright (c) 2014 Nick Durak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
